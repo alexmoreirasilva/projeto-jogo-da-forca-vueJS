48857bb7d934104133091e1ee20df0732a6e2874
Alex <alex.moreira@marknetdigital.com.br>
2022-08-08 22:51:54 -0300

    iniciando projeto de jogo da forca com vueJS

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>Jogo da Forca</h1>
+
+    <section id="inicio" v-if="tela === 'inicio'">
+
+
+      <Formulario v-if="etapa === 'palavra'"
+        title="Defina a palavra"
+        button="proximo"
+        :action="setPalavra"
+      />
+
+      <Formulario v-if="etapa === 'dica'"
+        title="Defina a dica"
+        button="Iniciar jogo :)"
+        :action="setDica"
+      />
+    </section>
+
+    <section id="jogo" v-if="tela === 'jogo'">Jogo</section>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './css/global.css';
+import Formulario from './components/Formulario';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      tela: 'inicio',
+      etapa: 'palavra',
+      palavra: '',
+      dica: ''
+    }
+  },
   components: {
-    HelloWorld
+    Formulario  
+  },
+  methods: {
+    setPalavra: function (palavra) {
+      this.palavra = palavra;
+      this.etapa = 'dica';
+    },
+    setDica: function (dica) {
+      this.dica = dica;
+      this.tela = 'jogo';
+      this.etapa = 'jogo';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
