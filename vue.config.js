a382a7994d742b0647214e97c6ffd5fcd315502c
Alex Silva <57776114+alex10237@users.noreply.github.com>
2022-09-22 20:54:38 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false // Close eslint verification
+  
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/projeto-jogo-da-forca-vueJS/'
+  : '/'
 })
