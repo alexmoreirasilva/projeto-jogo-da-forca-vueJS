287f756894cfb4ba64b386f873d0752b6d8dd335
Alex <alexmoreira64@gmail.com>
2024-02-20 14:21:21 -0300

    Correção dos pacotes e importações

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false // Close eslint verification
-  
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/projeto-jogo-da-forca-vueJS/'
-  : '/'
-})
